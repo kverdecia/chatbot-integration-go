9a4b8be3b8ae9473c1a65f8ad826d8c6fdd835c9
Karel Antonio Verdecia Ortiz <kverdeciaortiz@secureauth.com>
2019-12-18 11:25:55 -0300

    updated main.go

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"runtime"
+)
+
+func handle(w http.ResponseWriter, r *http.Request) {
+    os := runtime.GOOS;
+    fmt.Fprintf(w, "Hellow world from [%s] container!\n", os);
+}
 
 func main() {
     fmt.Println("Hello World!")
+    http.HandleFunc("/", handle)
+    if err := http.ListenAndServe(":8000", nil); err != nil {
+        log.Fatal(err)
+    }
 }
